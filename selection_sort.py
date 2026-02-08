a40a0b4b2790e106513a317d2f40114e3d74ce3c
Devam Singh <singhdevam68@gmail.com>
2026-02-08 22:50:14 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,6 @@
 nums = [55,67,58,34,12,61,912,23]
 
-def insertion_sort(arr):
+def selection_sort(arr):
     n=len(arr)
     for i in range(0,n):
         min_id=i
@@ -9,4 +9,4 @@ def insertion_sort(arr):
                 min_id=j
         arr[min_id],arr[i]=arr[i],arr[min_id]
     return arr
-print(insertion_sort(nums))
+print(selection_sort(nums))
