bb3a8f76ef65959201dbf62866c5695ca5a4f9d9
Devam Singh <singhdevam68@gmail.com>
2026-02-08 10:27:28 +0530

    Create palindrome_check_using_recursion.py

---
+++ palindrome_check_using_recursion.py
@@ -0,0 +1,13 @@
+s = 'mom'
+s1='nitin'
+def check_palindrome(s,left,right):
+    
+    if left>=right:
+        return True
+    if s[left]!=s[right]:
+        return False
+    return check_palindrome(s,left+1,right-1)
+    
+print(check_palindrome(s,0,len(s)-1))
+    
+    
