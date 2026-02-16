fe44c295e26b0e5727ba391e530c005754888c91
Devam Singh <singhdevam68@gmail.com>
2026-02-16 18:51:50 +0530

    Add lower_bound function for binary search
    Implement binary search for lower bound in a sorted array.

---
+++ binary_search_lower_bound.py
@@ -0,0 +1,15 @@
+arr = [1,1,1,2,3,3,5,6,7,7,7,9,12,12,13]
+def lower_bound(arr,target):
+    lb,n=-1,len(arr)
+    low=0
+    high=n-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid]>=target:
+            lb=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return lb
+print(lower_bound(arr,11))
+print(arr[lower_bound(arr,13)])
