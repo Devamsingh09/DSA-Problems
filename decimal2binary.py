f773837fd73bc8692bbe5ea9e846876a2762ba8e
Devam Singh <singhdevam68@gmail.com>
2026-02-24 19:02:03 +0530

    Add decimal to binary conversion function

---
+++ decimal2binary.py
@@ -0,0 +1,11 @@
+def convertDecimal2Binary(num:int):
+    result=""
+    while num>0:
+        if num%2==1:
+            result+='1'
+        else:
+            result+='0'
+        num=num//2
+    result=result[::-1]
+    return result
+print(convertDecimal2Binary(15))
